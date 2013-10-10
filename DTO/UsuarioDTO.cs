b3098a14c433082b1bae12d3fe7bd387589179c2
Jorge Luis Sarmiento Herrera <yorchsarmiento@gmail.com>
2013-10-10 12:38:00 -0500

    DTO & VO for user management

---
+++ DTO/UsuarioDTO.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstacionDB.DTO
+{
+    public class UsuarioDTO
+    {
+        private string usuario;
+
+        public string Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+        private bool isAdmin;
+
+        public bool IsAdmin
+        {
+            get { return isAdmin; }
+            set { isAdmin = value; }
+        }
+    }
+}
